1ae02fbb6883b4c5d960bae07ce270b01ce4b5d8
mae <matthew@pinholedesign.co.uk>
2016-01-21 16:51:44 +0000

    added first set of graphics state functions

--- state.go
+++ state.go
@@ -4,21 +4,39 @@ package ri
 import (
 	"sync"
 	"fmt"
+	"io"
+	"os"
 )
 
 var (
 	ErrInvalidContextHandle error = fmt.Errorf("Invalid Context Handle")
 	ErrContextAlreadyExists error = fmt.Errorf("Context Already Exists")
+	ErrNoActiveContext      error = fmt.Errorf("No Active Context")
 )
 
 type context struct {
 	Name string
+	writer io.Writer
+}
+
+func (ctx *context) Write(out string) {
+	ctx.writer.Write([]byte(out))
 }
 
 var internal struct {
 	contexts map[string]*context	
 	active *context
 	sync.Mutex
+}
+
+func write(out string) error {
+	internal.Lock()
+	defer internal.Unlock()
+	if internal.active == nil {
+		return ErrNoActiveContext
+	}
+	internal.active.Write(out)
+	return nil
 }
 
 func init() {
@@ -37,11 +55,33 @@ func Begin(name string) error {
 		return ErrContextAlreadyExists
 	}
 
-	ctx := &context{Name:name}
+	file,err := os.Create(name)
+	if err != nil {
+		return err
+	}
+
+	ctx := &context{Name:name,writer:file}
+	ctx.writer.Write([]byte("## RIB /* rigo */\n"))
+	
+	internal.contexts[name] = ctx
+	internal.active = ctx
+	return nil
+}
+
+func BeginWriter(name string,writer io.Writer) error {
+
+	internal.Lock()
+	defer internal.Unlock()
+
+	if _,exists := internal.contexts[name]; exists {
+		return ErrContextAlreadyExists 
+	}
+
+	ctx := &context{Name:name,writer:writer}
+	ctx.writer.Write([]byte("## RIB /* rigo */\n"))
 
 	internal.contexts[name] = ctx
 	internal.active = ctx
-
 	return nil
 }
 
@@ -49,6 +89,16 @@ func Begin(name string) error {
 func End() error {
 
 	/* TODO: finish up stream */
+
+	internal.Lock()
+	defer internal.Unlock()
+
+	if internal.active == nil {
+		return ErrNoActiveContext
+	}
+
+	delete(internal.contexts,internal.active.Name)
+	internal.active = nil
 	return nil
 }
 
@@ -81,8 +131,28 @@ func Context(handle RtContextHandle) error {
 	return nil
 }
 
+/* FrameBegin mark the beginning of a single frame of an animated sequenece */
+func FrameBegin(frame RtInt) error { 
+	return write(fmt.Sprintf("FrameBegin %d\n",frame))
+}
 
+/* FrameEnd mark the end of a single frame of an animated sequence */
+func FrameEnd() error {
+	return write("FrameEnd\n")
+}
 
+/* When WorldBegin is invoked, all rendering options are frozen */
+func WorldBegin() error {
+	return write("WorldBegin\n")
+}
 
+func WorldEnd() error {
+	return write("WorldEnd\n")
+}
+
+/* set the horizontal and vertical resolution (in pixels) of the image to be rendered */
+func Format(xresolution,yresolution RtInt,pixelaspectratio RtFloat) error {
+	return write(fmt.Sprintf("Format %d %d %f\n",xresolution,yresolution,pixelaspectratio))
+}
 
 
--- state_test.go
+++ state_test.go
@@ -1,5 +1,52 @@
 /* rigo/state_test.go */
 package ri
 
+import (
+	. "github.com/smartystreets/goconvey/convey"
+	"testing"
+	"bytes"
+)
 
+func Test_State(t *testing.T) {
+
+	Convey("Begin",t,func() {
+		Convey("twice with same name",func() {
+
+			So(Begin("test.rib"),ShouldBeNil)
+			So(Begin("test.rib"),ShouldEqual,ErrContextAlreadyExists)			
+		})
+	})
+
+	Convey("End",t,func() {
+		Convey("end twice",func() {
+			
+			So(End(),ShouldBeNil)
+			So(End(),ShouldEqual,ErrNoActiveContext)
+		})
+	})
+
+	Convey("BeginWriter/end",t,func() {
+		buf := bytes.NewBuffer(nil)
+		So(BeginWriter("test.rib",buf),ShouldBeNil)
+		So(End(),ShouldBeNil)
+		So(len(buf.Bytes()),ShouldNotEqual,0)	
+	})	
+
+
+	Convey("GetContext",t,func() {
+		Convey("get the active context",func() {
+
+			So(GetContext(),ShouldBeNil)
+		})
+
+		Convey("get valid context",func() {
+			
+			So(Begin("test.rib"),ShouldBeNil)
+			ctx := GetContext()
+			So(ctx,ShouldNotBeNil)
+			
+			So(Context(ctx),ShouldBeNil)
+		})
+	})
+}		
 
