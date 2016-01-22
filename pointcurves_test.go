fd8939c63c7797354feb2f885babbefa0761c93d
mae <matthew@pinholedesign.co.uk>
2016-01-22 20:58:27 +0000

    updated surfaces

--- pointcurves_test.go
+++ pointcurves_test.go
@@ -6,17 +6,16 @@ import (
 )
 
 
-func Test_Surfaces(t *testing.T) {
+func Test_PointCurves(t *testing.T) {
 
-	Convey("All Surfaces",t,func() {
+	Convey("All Point and Curves",t,func() {
 
 		ctx := New(nil)
 		So(ctx,ShouldNotBeNil)
 
-		So(ctx.Begin("output/surfaces.rib"),ShouldBeNil)
-		So(ctx.Comment("output from rigo, surfaces_test.go"),ShouldBeNil)
+		So(ctx.Begin("output/pointcurves.rib"),ShouldBeNil)
+		So(ctx.Comment("output from rigo, pointcurves_test.go"),ShouldBeNil)
 
-		So(ctx.SubdivisionMesh("catmull-clark",5,RtIntArray{4,4,4,4,4,4},RtIntArray{0,4,3,2,5,6,1,2,3},1,RtTokenArray{"interpolateboundary"},RtIntArray{0,0},RtIntArray{0},RtFloatArray{0}),ShouldBeNil)
 		So(ctx.Points(5,RtToken("P"),RtFloatArray{.5,-.5,-.5,0,.5,0,.5,0},RtToken("width"),RtFloatArray{.1,.12,.05,.02}),ShouldBeNil)
 		So(ctx.Curves("cubic",4,RtIntArray{4},"nonperiodic",RtToken("constantwidth"),RtFloat(0.1)),ShouldBeNil)
 
