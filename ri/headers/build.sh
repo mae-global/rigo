00fa659fdfff92156483c7aa8a804a357116d65e
mae <contact@mae.global>
2016-05-08 10:13:51 +0100

    added better bloom filter fit

--- ri/headers/build.sh
+++ ri/headers/build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 echo "building from $RMANTREE/include/ri.h..."
-go run ./gen.go -package=ri -target=$RMANTREE/include/ri.h
+go build
+./headers -package=ri -target=$RMANTREE/include/ri.h
+#go run ./gen.go -package=ri -target=$RMANTREE/include/ri.h
 gofmt -w ./out.txt
 echo "installing..."
 mv out.txt ../tokens.go
--- ri/headers/gen.go
+++ ri/headers/gen.go
@@ -12,7 +12,8 @@ import (
 	"text/template"
 	"flag"
 	"time"
-	"bytes"
+	"bytes"	
+	"sort"
 )
 
 const (
@@ -40,6 +41,9 @@ func main() {
 	ri := &Ri{}
 	ri.Tokens = make([]string,0)
 
+	hashes := make(map[string][]int,0)
+	
+
 	/* very crude parsing here */
 	for i,line := range lines {
 		if strings.Contains(line,"#define RI_VERSION") {
@@ -65,8 +69,91 @@ func main() {
 					ri.longest = len(token)
 				}
 			}
-		}
+		} else {
+			if strings.Contains(line,"RI_EXPORT") {
+			//	fmt.Printf("[%05d] parsing %s\n",i,line)
+				
+				parts := strings.Split(line," ")
+				/* ignore first 2 */
+				name := ""			
+				for _,p := range parts {
+					if strings.Contains(p,"Ri") {
+						name = p
+						break
+					}
+				}
+				if name == "" {
+					continue
+				}
+				
+				nameparts := strings.Split(name,"(")
+				name = strings.TrimPrefix(nameparts[0],"Ri")				
+				name = strings.Replace(name,";","",-1)						
+				
+				m := Hash(name)
+				hashes[name] = m
+
+				fmt.Printf("[%05d] Func = [%s] bloom@%v\n",i,name,m)
+			}
+		}			
+	}		
+
+
+	/* remove all the ...V functions */
+	toremove := make([]string,0)
+
+	for key,_ := range hashes {
+			if key[len(key) - 1] == 'V' {
+				toremove = append(toremove,key)
+			}
 	}
+
+	for _,key := range toremove {
+		delete(hashes,key)
+	}
+
+
+	/* find the best fit for the bloom filter */
+	size := 128
+	var bloom []int
+	var filter *BloomFilter
+
+	for {
+		bloom = make([]int,size)
+	
+		for _,h := range hashes {
+
+			for i := 0; i < len(h); i++ {
+				bloom[ h[i] % size ] ++		
+			}
+		}
+
+		filter = &BloomFilter{bloom,len(hashes)}	
+		fmt.Printf("\n============== [%05d] BloomFilter\n%s\n",size,filter.Print())
+
+		_,_,sparse := filter.Stats()
+
+		if !filter.IsMember("Alice","Fred","Eve") && filter.IsMember("Sphere","Translate") {
+			if sparse >= 0.8 {
+				break
+			}
+		} else {
+			fmt.Printf(">>> failed membership test\n")
+		}
+
+		size = size * 2		
+	}
+		
+	keys,bits := filter.Raw() 
+	ri.FilterData = bits
+	ri.FilterKeys = keys
+
+	ri.FilterKeysData = make([]string,0)
+	for key,_ := range hashes {
+		ri.FilterKeysData = append(ri.FilterKeysData,key)
+	}	
+
+	sort.Sort(ByAlpha(ri.FilterKeysData))
 
 	fmt.Printf("Parsed %d lines\n",len(lines))
 
@@ -99,6 +186,12 @@ func main() {
 
 }
 
+type ByAlpha []string
+
+func (b ByAlpha) Len() int { return len(b) }
+func (b ByAlpha) Less(i,j int) bool { return (b[i] < b[j]) }
+func (b ByAlpha) Swap(i,j int) { b[i],b[j] = b[j],b[i] }
+
 type T struct {
 	Version int
 	Date time.Time
@@ -111,6 +204,9 @@ type Ri struct {
 	Version int
 	Tokens []string
 	longest int
+	FilterData []int
+	FilterKeys int
+	FilterKeysData []string
 }
 
 func (ri *Ri) Token(t string) string {
@@ -137,6 +233,23 @@ const (
 {{range $token := .Ri.Tokens}}{{$token | Token}} RtToken = "{{$token | Value}}"
 {{end}}
 )
+
+var (
+	bloomFilterData = []int{ {{range $i := .Ri.FilterData}}{{$i}},{{end}} } 
+	bloomFilterKeysData = []string{ {{range $i := .Ri.FilterKeysData}}"{{$i}}",{{end}} }
+)
+
+const (
+	bloomFilterKeys int = {{.Ri.FilterKeys}}
+)
+
+func RiBloomFilter() *BloomFilter {
+	bits := make([]int,len(bloomFilterData))
+	copy(bits,bloomFilterData)
+	return &BloomFilter{bits,bloomFilterKeys}
+}
+
+
 
 /* EOF */
 `
